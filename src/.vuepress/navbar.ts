8b2cb9f3b06bdab78fbd03d9894bcf5003aab25b
bwf <2304576239@qq.com>
2024-10-29 15:01:52 +0800

    blog15

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -5,32 +5,46 @@ export default navbar([
   "/md/resume",
   {
     text: "Java",
-    icon: "book",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
   },
   {
-    text: "Spring",
-    icon: "book",
+    text: "Spring全家桶",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
+    items: [
+      {text: "Spring手搓",link: "xxx"},
+      {text: "SpringBoot",link: "xxx"},
+      {text: "SpringCloud",link: "xxx"}
+    ]
   },
   {
     text: "数据结构与算法",
-    icon: "book",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
   },
   {
-    text: "中间件",
-    icon: "book",
+    text: "Redis",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
   },
   {
-    text: "数据库",
-    icon: "book",
+    text: "MySQL",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
+    items: [
+      {text: "MySQL基础",link: "xxx"},
+      {text: "MySQL高级",link: "xxx"}
+    ]
   },
   {
     text: "Netty4.x",
-    icon: "book",
+    // icon: "book",
     link: "/md/dev-log/day01.md",
   },
+  {
+    text: "阿里云",
+    // icon: "book",
+    link: "/md/dev-log/day01.md",
+  }
 ]);
