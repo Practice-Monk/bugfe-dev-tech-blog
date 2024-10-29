72f6f0369ffddf7ecefda8f0176371c8796e60ae
bwf <2304576239@qq.com>
2024-10-29 16:17:16 +0800

    blog24

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -7,22 +7,31 @@ export default sidebar({
       text: "01",
       prefix: "md/dev-log/",
       children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
-    },
+    }
+    ,
+    // {
+    //   text: "02",
+    //   prefix: "md/dev-log/",
+    //   children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    // },
+    // {
+    //   text: "03",
+    //   prefix: "md/dev-log/",
+    //   children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    // },
+    // {
+    //   text: "04",
+    //   prefix: "md/dev-log/",
+    //   children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    // },
+  ],
+
+  "/Java" : [
     {
-      text: "02",
-      prefix: "md/dev-log/",
-      children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
-    },
-    {
-      text: "03",
-      prefix: "md/dev-log/",
-      children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
-    },
-    {
-      text: "04",
-      prefix: "md/dev-log/",
-      children: ["java01.md","java02.md","java03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
-    },
-  ]
+      text: "Java入门",
+      prefix: "md/java/",
+      children: ["java01.md","java02.md","java03.md"],
+    }
+  ],
 
 });
