5fdd47aaed07e1288e3d927b2a6780fe906241c3
bwf <2304576239@qq.com>
2024-10-29 14:39:51 +0800

    blog13

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -9,5 +9,23 @@ export default sidebar({
       prefix: "md/dev-log/",
       children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
     },
+    {
+      text: "02",
+      icon: "laptop-code",
+      prefix: "md/dev-log/",
+      children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    },
+    {
+      text: "03",
+      icon: "laptop-code",
+      prefix: "md/dev-log/",
+      children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    },
+    {
+      text: "04",
+      icon: "laptop-code",
+      prefix: "md/dev-log/",
+      children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+    },
   ],
 });
