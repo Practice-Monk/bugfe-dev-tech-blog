4b3d0e19e77bdd1bfde0f0bc8e7dd459da3816af
bwf <2304576239@qq.com>
2024-10-29 14:50:39 +0800

    blog14

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,7 +4,32 @@ export default navbar([
   "/",
   "/md/resume",
   {
-    text: "开发日志",
+    text: "Java",
+    icon: "book",
+    link: "/md/dev-log/day01.md",
+  },
+  {
+    text: "Spring",
+    icon: "book",
+    link: "/md/dev-log/day01.md",
+  },
+  {
+    text: "数据结构与算法",
+    icon: "book",
+    link: "/md/dev-log/day01.md",
+  },
+  {
+    text: "中间件",
+    icon: "book",
+    link: "/md/dev-log/day01.md",
+  },
+  {
+    text: "数据库",
+    icon: "book",
+    link: "/md/dev-log/day01.md",
+  },
+  {
+    text: "Netty4.x",
     icon: "book",
     link: "/md/dev-log/day01.md",
   },
